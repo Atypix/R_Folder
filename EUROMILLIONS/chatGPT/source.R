babc26f632c1c27bc31510f8013809fbddbe02a6
Atypix <sebodion@gmail.com>
2023-08-11 08:26:59 +0200

    Update source.R

--- EUROMILLIONS/chatGPT/source.R
+++ EUROMILLIONS/chatGPT/source.R
@@ -8,7 +8,7 @@ library(readr)
 # Préparation des données d'entraînement
 
 data <- read.csv("data_all.csv", sep=",", head=FALSE)
-data = tail(data, n = 100)
+data <- tail(data, n = 100)
 
 # Preprocessing des données
 data_scaled <- scale(data[,1:5])
