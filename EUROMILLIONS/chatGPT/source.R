a8ade713c9c613474e0fd31b2071be845336aeab
Atypix <sebodion@gmail.com>
2023-09-15 11:43:32 +0200

    Update source.R

--- EUROMILLIONS/chatGPT/source.R
+++ EUROMILLIONS/chatGPT/source.R
@@ -31,7 +31,7 @@ model %>% compile(
 )
 
 # Entrainement
-model %>% fit(data_input, data_target, epochs = 100, batch_size = 1)
+model %>% fit(data_input, data_target, epochs = 200, batch_size = 1)
 
 # Prediction
 next_input <- tail(data_scaled, n = 1)
